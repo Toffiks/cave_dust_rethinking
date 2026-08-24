d290d24b1ddbf6ec99f79b72bc44d9455906bdb6
Toffiks <Toffiks@users.noreply.github.com>
2026-08-25 03:55:40 +0500

    Merge advanced settings into main config screen

--- versions/forge-1.20.1/src/main/java/net/lizistired/cavedust/CaveDustConfigScreen.java
+++ versions/forge-1.20.1/src/main/java/net/lizistired/cavedust/CaveDustConfigScreen.java
@@ -1,10 +1,13 @@
 package net.lizistired.cavedust;
 
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractSliderButton;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+
+import java.util.function.IntConsumer;
 
 final class CaveDustConfigScreen extends Screen {
     private final Screen parent;
@@ -20,7 +23,7 @@ final class CaveDustConfigScreen extends Screen {
     protected void init() {
         config.reload();
         int left = (this.width - 200) / 2;
-        int row = (this.height - (20 + 2 * 24)) / 2;
+        int row = (this.height - (20 + 5 * 24)) / 2;
 
         Button enabled = Button.builder(enabledText(), button -> {
                     config.toggleEnabled();
@@ -33,9 +36,39 @@ final class CaveDustConfigScreen extends Screen {
         this.addRenderableWidget(enabled);
 
         row += 24;
-        this.addRenderableWidget(new CaveDustReleaseButton(left, row, 200, 20,
-                Component.translatable("menu.cavedust.title.advanced"),
-                () -> this.minecraft.setScreen(new CaveDustAdvancedConfigScreen(this))));
+        IntSlider amount = new IntSlider(left, row, 1, 100, config.particleMultiplier(),
+                "menu.cavedust.particlemultiplier", config::setParticleMultiplier);
+        amount.setTooltip(Tooltip.create(Component.translatable("menu.cavedust.particlemultiplier.tooltip")));
+        this.addRenderableWidget(amount);
+
+        row += 24;
+        Button particle = Button.builder(particleText(), button -> {
+                    config.iterateParticle();
+                    button.setMessage(particleText());
+                })
+                .bounds(left, row, 200, 20)
+                .tooltip(Tooltip.create(Component.translatable("menu.cavedust.particle.tooltip")))
+                .build();
+        this.addRenderableWidget(particle);
+
+        row += 24;
+        IntSlider radius = new IntSlider(left, row, 1, 50, config.width(),
+                "menu.cavedust.width", config::setWidth);
+        radius.setTooltip(Tooltip.create(Component.translatable("menu.cavedust.width.tooltip")));
+        this.addRenderableWidget(radius);
+
+        row += 24;
+        this.addRenderableWidget(Button.builder(Component.translatable("menu.cavedust.reset"), button -> {
+                    config.reset();
+                    enabled.setMessage(enabledText());
+                    enabled.setTooltip(enabledTooltip());
+                    amount.setCurrent(config.particleMultiplier());
+                    particle.setMessage(particleText());
+                    radius.setCurrent(config.width());
+                })
+                .bounds(left, row, 200, 20)
+                .tooltip(Tooltip.create(Component.translatable("menu.cavedust.reset.tooltip")))
+                .build());
 
         row += 24;
         this.addRenderableWidget(Button.builder(
@@ -52,6 +85,10 @@ final class CaveDustConfigScreen extends Screen {
         return Tooltip.create(Component.translatable("menu.cavedust.global.tooltip." + config.enabled()));
     }
 
+    private Component particleText() {
+        return Component.literal("Particle: " + config.particleName());
+    }
+
     @Override
     public void onClose() {
         this.minecraft.setScreen(parent);
@@ -62,5 +99,45 @@ final class CaveDustConfigScreen extends Screen {
         this.renderBackground(graphics);
         super.render(graphics, mouseX, mouseY, partialTick);
         graphics.drawCenteredString(this.font, this.title, this.width / 2, 30, 0xFFFFFF);
+    }
+
+    private static final class IntSlider extends AbstractSliderButton {
+        private final int minimum;
+        private final int maximum;
+        private final String translationKey;
+        private final IntConsumer consumer;
+
+        private IntSlider(int x, int y, int minimum, int maximum, int current,
+                          String translationKey, IntConsumer consumer) {
+            super(x, y, 200, 20, Component.empty(), normalize(minimum, maximum, current));
+            this.minimum = minimum;
+            this.maximum = maximum;
+            this.translationKey = translationKey;
+            this.consumer = consumer;
+            this.updateMessage();
+        }
+
+        private int current() {
+            return minimum + (int) Math.round(this.value * (maximum - minimum));
+        }
+
+        private void setCurrent(int current) {
+            this.value = normalize(minimum, maximum, current);
+            this.updateMessage();
+        }
+
+        @Override
+        protected void updateMessage() {
+            this.setMessage(Component.translatable(translationKey, current()));
+        }
+
+        @Override
+        protected void applyValue() {
+            consumer.accept(current());
+        }
+
+        private static double normalize(int minimum, int maximum, int value) {
+            return (double) (value - minimum) / (double) (maximum - minimum);
+        }
     }
 }
